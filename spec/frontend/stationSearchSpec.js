4194ae93663cd289595dceed8be1207fd66239b9
Andrea Mazzarella <andrea.mazzarella89@gmail.com>
2016-07-13 07:17:44 +0100

    Station Search now under unit test; refactoring needed + more tests

--- spec/frontend/stationSearchSpec.js
+++ spec/frontend/stationSearchSpec.js
@@ -1,7 +1,21 @@
-var expect = require('chai').expect;
+'use strict';
 
-describe('Station Search module', function() {
-  it('test drive', function() {
-    expect(5).to.equal(5);
-  })
+var expect        = require('chai').expect;
+var sinon         = require('sinon');
+var StationSearch = require('../../lib/src/stationSearch').StationSearch;
+var ApiHandler    = require('../../lib/src/ApiHandler').ApiHandler;
+
+describe('StationSearch', function() {
+
+  describe('#search', function() {
+  
+    it('calls the injected getHandler with the correct url', function() {
+      var mockGet = sinon.stub(ApiHandler, 'get');
+      var stationSearch = new StationSearch(ApiHandler);
+      stationSearch.search('wharf');
+      expect(mockGet.calledWith('https://api.tfl.gov.uk/StopPoint/Search/wharf?modes=tube&faresOnly=False&app_id=86edcb7f&app_key=d623423387aeb5205f919ef9f8549779')).to.equal(true);
+    });
+
+  });
+
 });
